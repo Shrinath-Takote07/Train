5f67b28386ce1c8bab637c316a762b6ef3619679
Shrinath takote <shrinathtakote163@gmail.com>
2026-08-18 23:02:18 +0530

    Update vite.config.js

--- Frontend/my-project/vite.config.js
+++ Frontend/my-project/vite.config.js
@@ -4,4 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://train-u5pc.vercel.app",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/socket.io": {
+        target: "https://train-u5pc.vercel.app",
+        changeOrigin: true,
+        ws: true,
+        secure: false,
+      },
+    },
+  },
 });
+
